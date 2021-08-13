161b5880029e96607efe71a197701b3b4f791683
em_qr <emquinn-reed@hotmail.com>
2021-08-13 16:13:16 -0400

    Fetch words and remaining guesses

--- js/script.js
+++ js/script.js
@@ -8,8 +8,20 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 
-const word = "magnolia";
+let word = "magnolia";
 const guessedLetters = [];
+let remainingGuesses = 8;
+
+const getWord = async function () {
+  const res = await fetch ("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+  const words = await res.text();
+  const wordArray = words.split("\n");
+  const randomIndex = Math.floor(Math.random() * wordArray.length);
+  word = wordArray[randomIndex].trim();
+  placeholder(word);
+  };
+
+getWord();
 
 
 const placeholder = function (word) {
@@ -62,6 +74,7 @@ const validateInput = function (input){
         } else {
             guessedLetters.push(guess);
             console.log(guessedLetters);
+            updateGuessesRemaining(guess);
             showGuessedLetters();
             updateWordInProgress (guessedLetters);
         }  
@@ -92,9 +105,28 @@ const updateWordInProgress = function (guessedLetters){
     checkIfWin();
   };
 
+  const updateGuessesRemaining = function (guess) {
+   const upperWord = word.toUpperCase();
+   if (!upperWord.includes(guess)) {
+     message.innerText = `Sorry, the word has no ${guess}.`;
+     remainingGuesses -= 1;
+   } else {
+     message.innerText = `Good guess! the word has the letter ${guess}.`;
+   }
+
+   if (remainingGuesses === 0) {
+     message.innerHTML = `Game over! the word was <span class = "highlight">${word}</span>.`;
+   } else if (remainingGuesses === 1 ) {
+     remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
+   } else {
+     remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+   }
+  };
+
 const checkIfWin = function () {
    if (word.toUpperCase() === wordInProgress.innerText) {
      message.classList.add("win");
      message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
    }
 };
+
